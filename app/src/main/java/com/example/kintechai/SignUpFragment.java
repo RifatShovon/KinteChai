d6fdbeec63536402be8d9377b5d8159aa8b5400d
OahidZihad <oahidzihad1@gmail.com>
2020-02-14 12:44:01 +0600

    Completing SignUp System

--- app/src/main/java/com/example/kintechai/SignUpFragment.java
+++ app/src/main/java/com/example/kintechai/SignUpFragment.java
@@ -1,18 +1,39 @@
 package com.example.kintechai;
 
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.kintechai.R.anim;
+import com.example.kintechai.R.id;
+import com.example.kintechai.R.layout;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -28,31 +49,189 @@ public class SignUpFragment extends Fragment {
     private TextView alreadyHaveAnAccount;
     private FrameLayout parentFrameLayout;
 
+    private EditText emailId;
+    private EditText userName;
+    private EditText password;
+    private EditText confirmPassword;
+
+    private ImageButton closeBtn;
+    private Button signUpBtn;
+
+    private FirebaseAuth firebaseAuth;
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
-        alreadyHaveAnAccount = view.findViewById(R.id.tv_already_have_an_account);
-        parentFrameLayout = getActivity().findViewById(R.id.register_framelayout);
+        View view = inflater.inflate(layout.fragment_sign_up, container, false);
+
+        parentFrameLayout = getActivity().findViewById(id.register_framelayout);
+
+        alreadyHaveAnAccount = view.findViewById(id.tv_already_have_an_account);
+        emailId = view.findViewById(id.sign_up_email);
+        userName = view.findViewById(id.sign_up_username);
+        password = view.findViewById(id.sign_up_password);
+        confirmPassword = view.findViewById(id.sign_up_confirm_password);
+
+        closeBtn = view.findViewById(id.sign_up_close);
+        signUpBtn = view.findViewById(id.sign_up_btn);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        alreadyHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+        alreadyHaveAnAccount.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 setFragment(new SignInFragment());
             }
         });
+
+        emailId.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @RequiresApi(api = VERSION_CODES.O)
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        userName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @RequiresApi(api = VERSION_CODES.O)
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @RequiresApi(api = VERSION_CODES.O)
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        confirmPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @RequiresApi(api = VERSION_CODES.O)
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkEmailAndPassword();
+            }
+        });
     }
+
     private void setFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left,R.anim.slideout_from_right);
+        fragmentTransaction.setCustomAnimations(anim.slide_from_left, anim.slideout_from_right);
         fragmentTransaction.replace(parentFrameLayout.getId(), fragment);
         fragmentTransaction.commit();
+    }
+
+    private void checkInputs() {
+        if (!TextUtils.isEmpty(emailId.getText())) {
+            if (!TextUtils.isEmpty(userName.getText())) {
+                if (!TextUtils.isEmpty(password.getText()) && password.length() >= 8) {
+                    if (!TextUtils.isEmpty(confirmPassword.getText())) {
+                        signUpBtn.setEnabled(true);
+                        signUpBtn.setTextColor(Color.rgb(255, 255, 255));
+                    } else {
+                        signUpBtn.setEnabled(false);
+                        signUpBtn.setTextColor(Color.argb(50,255, 255, 255));
+                    }
+                } else {
+                    signUpBtn.setEnabled(false);
+                    signUpBtn.setTextColor(Color.argb(50,255, 255, 255));
+                }
+            } else {
+                signUpBtn.setEnabled(false);
+                signUpBtn.setTextColor(Color.argb(50,255, 255, 255));
+            }
+        } else {
+            signUpBtn.setEnabled(false);
+            signUpBtn.setTextColor(Color.argb(50,255,255,255));
+        }
+    }
+    private void checkEmailAndPassword(){
+        if (emailId.getText().toString().matches(emailPattern)){
+            if (password.getText().toString().equals(confirmPassword.getText().toString())){
+
+                signUpBtn.setEnabled(false);
+                signUpBtn.setTextColor(Color.argb(50,255, 255, 255));
+
+                firebaseAuth.createUserWithEmailAndPassword(emailId.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Intent mainIntent = new Intent(getActivity(),Main2Activity.class);
+                                    startActivity(mainIntent);
+                                    getActivity().finish();
+                                }else{
+                                    signUpBtn.setEnabled(true);
+                                    signUpBtn.setTextColor(Color.rgb(255, 255, 255));
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }else {
+                confirmPassword.setError("Password Doesn't Matched!");
+            }
+        }else {
+            emailId.setError("Invalid Email!");
+        }
     }
 }
