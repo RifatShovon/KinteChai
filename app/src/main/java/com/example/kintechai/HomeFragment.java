010a1f894fa33e68a42f37967a816575daa639b1
OahidZihad <oahidzihad1@gmail.com>
2020-02-23 04:46:31 +0600

    Add Banner Slider

--- app/src/main/java/com/example/kintechai/HomeFragment.java
+++ app/src/main/java/com/example/kintechai/HomeFragment.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
+import android.os.Handler;
+import android.transition.Slide;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
@@ -27,6 +33,17 @@ public class HomeFragment extends Fragment {
 
     private RecyclerView categoryRecyclerView;
     private CategoryAdapter categoryAdapter;
+
+    ////////// Banner Slider
+
+    private ViewPager bannerSliderViewPager;
+    private List<SliderModel> sliderModelList;
+    private int currentPage = 2;
+    private Timer timer;
+    final private long DELAY_TIME = 2000;
+    final private long PERIOD_TIME = 2000;
+
+    ////////// Banner Slider
 
 
     @Override
@@ -56,7 +73,108 @@ public class HomeFragment extends Fragment {
         categoryRecyclerView.setAdapter(categoryAdapter);
         categoryAdapter.notifyDataSetChanged();
 
+        //////////Banner Slider
+
+        bannerSliderViewPager = view.findViewById(R.id.banner_slider_view_pager);
+
+        sliderModelList = new ArrayList<SliderModel>();
+        sliderModelList.add(new SliderModel(R.mipmap.home_icon));
+        sliderModelList.add(new SliderModel(R.mipmap.custom_error_icon));
+
+        sliderModelList.add(new SliderModel(R.mipmap.forget_email_icon2));
+        sliderModelList.add(new SliderModel(R.mipmap.forget_email_icon1));
+        sliderModelList.add(new SliderModel(R.mipmap.logo_icon1));
+        sliderModelList.add(new SliderModel(R.mipmap.ic_launcher));
+        sliderModelList.add(new SliderModel(R.mipmap.cart_black));
+        sliderModelList.add(new SliderModel(R.mipmap.profile_placeholder));
+        sliderModelList.add(new SliderModel(R.mipmap.home_icon));
+        sliderModelList.add(new SliderModel(R.mipmap.custom_error_icon));
+        sliderModelList.add(new SliderModel(R.drawable.banner));
+
+        sliderModelList.add(new SliderModel(R.mipmap.forget_email_icon2));
+        sliderModelList.add(new SliderModel(R.mipmap.forget_email_icon1));
+
+        SliderAdapter sliderAdapter = new SliderAdapter(sliderModelList);
+        bannerSliderViewPager.setAdapter(sliderAdapter);
+        bannerSliderViewPager.setClipToPadding(false);
+        bannerSliderViewPager.setPageMargin(20);
+
+        ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if(state == ViewPager.SCROLL_STATE_IDLE){
+                    pageLooper();
+                }
+            }
+        };
+        bannerSliderViewPager.addOnPageChangeListener(onPageChangeListener);
+
+        startBannerSlideShow();
+
+        bannerSliderViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                pageLooper();
+                stopBannerSlideShow();
+                if (event.getAction() == MotionEvent.ACTION_UP){
+                    startBannerSlideShow();
+                }
+                return false;
+            }
+        });
+
+        //////////Banner Slider
+
         return view;
     }
+    //////////Banner Slider
+
+    private  void pageLooper(){
+        if (currentPage == sliderModelList.size() - 2){
+            currentPage = 2;
+            bannerSliderViewPager.setCurrentItem(currentPage,false);
+        }
+        if (currentPage == 1){
+            currentPage = sliderModelList.size() - 3;
+            bannerSliderViewPager.setCurrentItem(currentPage,false);
+        }
+    }
+
+    private void startBannerSlideShow(){
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage >= sliderModelList.size()){
+                    currentPage = 1;
+                }
+                bannerSliderViewPager.setCurrentItem(currentPage++,true);
+            }
+        };
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        },DELAY_TIME,PERIOD_TIME);
+    }
+
+    private void stopBannerSlideShow(){
+        timer.cancel();
+    }
+
+    //////////Banner Slider
+
 
 }
